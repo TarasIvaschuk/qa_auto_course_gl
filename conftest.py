3ad5d3e49dbba55fa37aa06b533d5a944e1b1ab8
Taras Ivashchuk <tarasivashchuk@gmail.com>
2023-03-16 15:06:29 +0200

    added configuration for pytest

---
+++ conftest.py
@@ -0,0 +1,22 @@
+import pytest
+
+class User:
+  def __init__ (self, age=33):
+    self.age = age
+
+  def remove(self):
+    self.age = None
+
+
+@pytest.fixture
+def user():
+  # before test
+  print('User created')
+  user = User(42)
+
+  # pass user object to test
+  yield user
+
+  # after test
+  print('User removed')
+  user.remove()
