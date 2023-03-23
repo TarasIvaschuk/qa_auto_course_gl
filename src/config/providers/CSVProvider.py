2b86843cb3eef9fe55e946afec5dadbf3019fc64
Taras Ivashchuk <tarasivashchuk@gmail.com>
2023-03-23 18:34:52 +0200

    update reading from CSV

--- src/config/providers/CSVProvider.py
+++ src/config/providers/CSVProvider.py
@@ -1,32 +1,31 @@
-import os 
+import os
+import csv
 
 filePath = os.path.realpath(__file__)
 currentDir = os.path.dirname(filePath)
 
+
 class CSVProvider:
     def __init__(self):
-        # todo 
         self._f = f'{currentDir}/data.csv'
-        self._separator = ","
-        self._delim = "\""
         self._d = {}
         self._createDict()
 
-    def _processRow(self, r):
-        k,v = [
-            c.strip().strip(self._delim)
-            for c in r.split(self._separator)
-        ]
-        
-        return k,v
-
     def _createDict(self):
         with open(self._f) as f:
-            # skip the header
-            next(f)
-            for row in f:
-                k,v = self._processRow(row)
+            csv.register_dialect(
+                'custom',
+                delimiter=",",
+                quotechar="\"",
+                skipinitialspace=True,
+                escapechar="\\"
+            )
+            text = csv.reader(f, dialect="custom")
+            # skip the headers
+            next(text)
+            for row in text:
+                k, v = row
                 self._d[k] = v
 
-    def  getVal(self,k):
-        return self._d.get(k,None)
+    def getVal(self, k):
+        return self._d.get(k, None)
