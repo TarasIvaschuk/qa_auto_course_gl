f666b93b2c696fe3c152c73afc57ae2d66564436
Taras Ivashchuk <tarasivashchuk@gmail.com>
2023-04-05 17:00:16 +0300

    updated tests

--- tests/test_test.py
+++ tests/test_test.py
@@ -1,12 +1,12 @@
-from src.config.conf import Config
+from src.config.conf import config
 
-def test_user_age_is_43(user):
-  print('check if user age is 43')
-  assert user.age == 43
+def test_user_age_is_not_43(user):
+  print('check if user age is not 43')
+  assert user.age != 43
 
 def test_user_age_is_42(user):
   print('check if user age is 42')
   assert user.age == 42
 
 def test_http_request():
-   assert Config().get('BASE_URL') == 'test.test.com'
+   assert config.get('BASE_URL') == 'test.test.com'
