371b34af1ed9deb9d07c8a00b58d9a80507b54c9
Taras Ivashchuk <tarasivashchuk@gmail.com>
2023-04-12 21:55:00 +0300

    cleanup github tests moved to other directory

--- tests/test_test.py
+++ tests/test_test.py
@@ -14,21 +14,3 @@ def test_user_age_is_42(user):
 def test_base_test_url():
     assert config.get('BASE_TEST_URL') == 'test_url'
 
-
-def test_search_existing_repos_on_github():
-    response = github_client.search_repos_by_name('TarasIvaschuk')
-    assert response.json()['total_count'] > 0
-    assert response.status_code == 200
-
-
-def test_search_existing_repos_on_github():
-    response = github_client.search_repos_by_name('sergii-butenko-gl')
-    assert response.json()['total_count'] > 0
-    assert response.status_code == 200
-
-
-def test_seach_non_existing_repos_on_github():
-    # github won't let you have the repository with name starting with $$
-    response = github_client.search_repos_by_name('$$')
-    assert response.status_code == 422
-    
