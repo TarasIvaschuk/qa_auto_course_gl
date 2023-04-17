abe8bb7ed2e3476a9059a4abc2595d9a84136da7
Taras Ivashchuk <tarasivashchuk@gmail.com>
2023-04-17 16:09:07 +0300

    added constants

---
+++ tests/api/constants.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class Github_constants(Enum):
+  REPO_OWNER = 'TarasIvaschuk'
+  NEW_REPO = 'taras_ivashchuk_test'
+  REPO = 'qa_auto_course_gl'
+
+
+
+class Time_constants(Enum):
+  SEC2 = 2
