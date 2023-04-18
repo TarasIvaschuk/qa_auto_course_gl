26ea0366c71535c638515b9456498fde4cb34c99
Taras Ivashchuk <tarasivashchuk@gmail.com>
2023-04-18 16:52:38 +0300

    updated sign in test

--- tests/ui/test_github_sign_in.py
+++ tests/ui/test_github_sign_in.py
@@ -1,14 +1,10 @@
-import pytest  
-from src.applications.ui.github_ui_app import githubui
-
-@pytest.fixture
-def browser():
-  githubui.launch()
-  yield githubui
-  githubui.close()
+import pytest
+# from src.applications.ui.github_ui_app import github_ui
+from src.applications.ui.page_objects.sign_in_page import Sign_in_page
 
 
-def test_failed_login(browser):
-  browser.go_to_login_page()
-  browser.try_login()
-  assert browser.check_error_message()
+def test_failed_login(github_ui_app):
+  sign_in_page = github_ui_app.SIGN_IN_PAGE
+  sign_in_page.go_to()
+  sign_in_page.try_sign_in('username', 'password')
+  assert sign_in_page.check_error_message()
