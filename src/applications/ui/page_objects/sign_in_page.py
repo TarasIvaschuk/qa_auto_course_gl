4ea79a24f35c6119ddf64e4585cb9a710ab860cd
Taras Ivashchuk <tarasivashchuk@gmail.com>
2023-04-18 16:53:37 +0300

    created a page for sign in

---
+++ src/applications/ui/page_objects/sign_in_page.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from src.config.conf import CONFIG
+from selenium.webdriver.common.by import By
+import time
+
+
+class Sign_in_page:
+  def __init__(self, driver) -> None:
+    self.USERNAME_FLD = 'login_field' # id
+    self.PASSWORD_FLD = 'password' #id
+    self.SIGN_IN_BTN = 'password' # name
+    self.URL = '/login'
+    self.driver = driver
+  
+  def go_to(self):
+    self.driver.get(CONFIG.get('GITHUB_BASE_URL_UI')+ self.URL)
+
+  def try_sign_in(self, username, password):
+    username_fld = self.driver.find_element(By.ID, self.USERNAME_FLD)
+    username_fld.send_keys(username)
+    time.sleep(2)
+
+
+    password_fld = self.driver.find_element(By.ID, self.PASSWORD_FLD)
+    password_fld.send_keys(password)
+    time.sleep(2)
+
+
+    sign_in_btn  = self.driver.find_element(By.NAME, self.SIGN_IN_BTN)
+    sign_in_btn.click()
+    time.sleep(2)
+
+    return self
+
+  def check_error_message(self):
+    # todo
+    return "Error" == "Error"
