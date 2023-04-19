cbcce4274e77fc8fcc9cc0a63721dd6c0bb287d6
Taras Ivashchuk <tarasivashchuk@gmail.com>
2023-04-19 15:39:56 +0300

    Refactoring: tests functions encapsulated into test class

--- tests/api/github_REST_test.py
+++ tests/api/github_REST_test.py
@@ -1,67 +1,91 @@
 from src.applications.api.github_client import github_client
-from tests.api.constants import Github_constants, Time_constants
+from tests.api.constants import Time_constants
 import time
 
-
-def test_search_existing_repos():
-    response = github_client.search_repo(Github_constants.REPO.value)
-    assert response.status_code == 200
-    assert response.json()['total_count'] > 0
-
-
-def test_seach_non_existing_repos():
-    # github won't let you have the repository with name starting with $$
-    response = github_client.search_repo(Github_constants.NEW_REPO.value)
-    assert response.status_code == 200
-    assert response.json()['total_count'] == 0
-
-
-def test_create_new_repo(g_repo_description):
-    response = github_client.create_repo(
-        Github_constants.NEW_REPO.value, description=g_repo_description['desc'])
-    assert response.status_code == 201
-    assert response.json()[
-        'full_name'] == f'{Github_constants.REPO_OWNER.value}/{Github_constants.NEW_REPO.value}'
+class TestGitHubRestAPI:
+    '''
+    To share variables between each tests
+    is not possible with the help of class attributes
+    so global fixture (conftest.py) is used instead 
+    '''
+    RESPONSE = 'response'
+    TOTAL_COUNT = 'total_count'
+    FULL_NAME = 'full_name'
+    DESCRIPTION = 'description'
+    ITEMS = 'items'
+    REPO_OWNER = 'TarasIvaschuk'
+    NEW_REPO = 'taras_ivashchuk_test'
+    REPO = 'qa_auto_course_gl'
 
 
-def test_search_newly_created_repo(g_repo_description):
-    time.sleep(Time_constants.SEC2.value)
-    response = github_client.search_repo(Github_constants.NEW_REPO.value)
-    assert response.status_code == 200
-    assert response.json()['total_count'] > 0
-    for d in response.json()['items']:
-        if 'description' in d:
-            desc = d['description']
+    def test_search_existing_repo_status_code_success(self, global_vars):
+        response = github_client.search_repo(self.REPO)
+        global_vars[self.RESPONSE] = response.json()
+        assert response.status_code == 200
 
-    assert desc == g_repo_description['desc']
+    def test_search_existing_repo_total_count_gt_0(self ,global_vars):
+        assert global_vars.get(self.RESPONSE).get(self.TOTAL_COUNT) > 0
 
+    def test_seach_non_existing_repo_status_code_success(self ,global_vars):
+        response = github_client.search_repo(self.NEW_REPO)
+        global_vars[self.RESPONSE] = response.json()
+        assert response.status_code == 200
 
-def test_update_description_of_newly_created_repo(g_repo_description):
-    g_repo_description['desc'] = 'updated with python'
-    data = {"description": g_repo_description['desc']}
-    response = github_client.update_repo(
-        Github_constants.REPO_OWNER.value, Github_constants.NEW_REPO.value, data)
-    assert response.status_code == 200
+    def test_seach_non_existing_repo_count_eq_0(self ,global_vars):
+        assert global_vars.get(self.RESPONSE).get(self.TOTAL_COUNT) == 0
 
+    def test_create_new_repo_status_code_success(self ,global_vars):
+        response = github_client.create_repo(
+            self.NEW_REPO, description=global_vars.get(self.DESCRIPTION))
+        assert response.status_code == 201
 
-def test_description_of_newly_created_repo_is_updated(g_repo_description):
-    response = github_client.search_repo(Github_constants.NEW_REPO.value)
-    assert response.status_code == 200
-    for d in response.json()['items']:
-        if 'description' in d:
-            desc = d['description']
+    def test_search_new_repo_total_count_gt_0(self ,global_vars):
+        time.sleep(Time_constants.SEC2.value)
+        response = github_client.search_repo(self.NEW_REPO)
+        global_vars[self.RESPONSE] = response.json()
+        assert global_vars.get(self.RESPONSE).get(self.TOTAL_COUNT) > 0
 
-    assert desc == g_repo_description['desc']
+    def test_new_repo_full_name(self, global_vars):
+        response = global_vars.get(self.RESPONSE)
+        for d in response.get(self.ITEMS):
+            if self.FULL_NAME in d:
+                full_name = d.get(self.FULL_NAME)
 
+        assert full_name == f'{self.REPO_OWNER}/{self.NEW_REPO}'
 
-def test_delete_newly_created_repo():
-    response = github_client.delete_repo(
-        Github_constants.REPO_OWNER.value, Github_constants.NEW_REPO.value)
-    assert response.status_code == 204
+    def test_new_repo_description(self ,global_vars):
+        time.sleep(Time_constants.SEC2.value)
+        response = global_vars.get(self.RESPONSE)
+        for d in response.get(self.ITEMS):
+            if self.DESCRIPTION in d:
+                desc = d.get(self.DESCRIPTION)
 
+        assert desc == global_vars.get(self.DESCRIPTION)
 
-def test_search_deleted_newly_created_repo():
-    time.sleep(Time_constants.SEC2.value)
-    response = github_client.search_repo(Github_constants.NEW_REPO.value)
-    assert response.status_code == 200
-    assert response.json()['total_count'] == 0
+    def test_update_new_repo_description_status_code_success(self,global_vars):
+        global_vars[self.DESCRIPTION] = 'updated with python'
+        data = {self.DESCRIPTION: global_vars.get(self.DESCRIPTION)}
+        response = github_client.update_repo(
+            self.REPO_OWNER, self.NEW_REPO, data)
+        global_vars[self.RESPONSE] = response.json()
+        assert response.status_code == 200
+
+    def test_new_repo_updated_description(self,global_vars):
+        response = global_vars.get(self.RESPONSE)
+        assert response.get(self.DESCRIPTION) == global_vars.get(
+            self.DESCRIPTION)
+
+    def test_delete_new_repo_status_code_success(self):
+        response = github_client.delete_repo(
+            self.REPO_OWNER, self.NEW_REPO)
+        assert response.status_code == 204
+
+    def test_search_deleted_new_repo_status_code_success(self ,global_vars):
+        time.sleep(Time_constants.SEC2.value)
+        response = github_client.search_repo(self.NEW_REPO)
+        global_vars[self.RESPONSE] = response.json()
+        assert response.status_code == 200
+
+    def test_search_deleted_new_repo_count_eq_0(self,global_vars):
+        time.sleep(Time_constants.SEC2.value)
+        assert global_vars.get(self.RESPONSE).get(self.TOTAL_COUNT) == 0
