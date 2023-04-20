f224f64e857531169ab4c065f351cd0c2cd60b1c
Taras Ivashchuk <tarasivashchuk@gmail.com>
2023-04-21 00:02:07 +0300

    added unpacking to selectors in sign in page

--- src/applications/ui/page_objects/sign_in_page.py
+++ src/applications/ui/page_objects/sign_in_page.py
@@ -9,9 +9,9 @@ from src.applications.constants import GitHubURL
 
 class Sign_in_page:
   def __init__(self, app) -> None:
-    self.USERNAME_FLD = 'login_field' # id
-    self.PASSWORD_FLD = 'password' #id
-    self.SIGN_IN_BTN = 'password' # name
+    self.USERNAME_FLD = By.ID,'login_field'
+    self.PASSWORD_FLD = By.ID, 'password'
+    self.SIGN_IN_BTN = By.NAME,'password'
     self.URL = '/login'
     self.app = app
   
@@ -19,17 +19,17 @@ class Sign_in_page:
     self.app.driver.get(GitHubURL.UI.BASE_URL+ self.URL)
 
   def try_sign_in(self, username, password):
-    username_fld = self.app.driver.find_element(By.ID, self.USERNAME_FLD)
+    username_fld = self.app.driver.find_element(*self.USERNAME_FLD)
     username_fld.send_keys(username)
     time.sleep(2)
 
 
-    password_fld = self.app.driver.find_element(By.ID, self.PASSWORD_FLD)
+    password_fld = self.app.driver.find_element(*self.PASSWORD_FLD)
     password_fld.send_keys(password)
     time.sleep(2)
 
 
-    sign_in_btn  = self.app.driver.find_element(By.NAME, self.SIGN_IN_BTN)
+    sign_in_btn  = self.app.driver.find_element(*self.SIGN_IN_BTN)
     sign_in_btn.click()
     time.sleep(2)
 
