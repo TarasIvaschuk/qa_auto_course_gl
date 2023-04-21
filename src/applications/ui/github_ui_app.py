30575ad5d6e313fbbedc9554d69d61bbf720a6aa
Taras Ivashchuk <tarasivashchuk@gmail.com>
2023-04-21 17:51:32 +0300

    fixed imports

--- src/applications/ui/github_ui_app.py
+++ src/applications/ui/github_ui_app.py
@@ -1,9 +1,9 @@
 from src.applications.ui.page_objects.sign_in_page import Sign_in_page
 from src.config.conf import CONFIG
 from src.applications.constants import GitHubURL
-from src.applications.ui.base_app import BaseAPP
+from src.applications.ui.selenium_wrapper import SeleniumWrapper
 
-class GitHubUI(BaseAPP):
+class GitHubUI(SeleniumWrapper):
   def __init__(self, driver) -> None:
     super().__init__(driver)
     self.SIGN_IN_PAGE = Sign_in_page(self)
